c540091d83b5ae69c010e9297ea070065f028ed7
AaronDennis07 <aarondennisbv01@gmail.com>
2023-11-02 01:40:23 +0530

    navbar 2 iter

--- client/app/(home)/layout.js
+++ client/app/(home)/layout.js
@@ -1,27 +1,37 @@
 
 import '@/app/global.css'
 import Image from 'next/image'
-
+import Link from 'next/link'
+import {FaRegPenToSquare} from "react-icons/fa6"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body >
-      <div className="navbar p-0 bg-slate-900 text-white">
-  <div className=" px-6 align-middle h-20 text-center mt-0 p-0 ">
-    <Image alt='some' src="/title.svg" width={150} height={100}/>
+      <div className="navbar p-0 bg-slate-950 h-16 items-center">
+  <div className=" px-4 h-20 text-center  ">
+    <Image alt='some' src="/title.svg" width={130} height={100}/>
 
   </div>
   <div className='w-full'>
-    <div className="form-control w-full mr-32">
-      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+    <div className="form-control w-5/6 pl-8  mr-4">
+      <input type="text" placeholder="Search"  className="h-10 input input-bordered w-ful" />
     </div>
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src="/next.svg" />
+   
+    <Link href="/" className='flex items-center gap-x-1 text-white w-16  h-full mr-2'>
+      <FaRegPenToSquare className='pb-1  h-10 w-10'/>
+      <p className='text-md'>Write</p>
+    </Link>
+    <div className="w-52 h-32  flex items-center   dropdown dropdown-end ">
+      <label tabIndex={0} className="ml-2 btn btn-ghost btn-circle avatar mr-2">
+        
+        <div className=" rounded-full">
+          <Image src="/profile.jpg"  height={45} width={45}/>
         </div>
+        
+    
       </label>
+      <p className='text-white'>Aaron Dennis</p>
       <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
         <li>
           <a className="justify-between">
