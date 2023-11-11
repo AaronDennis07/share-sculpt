c4877484bf3885842fefb6403e543096f3e688b2
AaronDennis07 <aarondennisbv01@gmail.com>
2023-11-11 22:52:18 +0530

    version 1 front-end 1st phase

---
+++ client/app/components/ShowBlog.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+
+const ShowBlog = () => {
+    return (
+        <section className='w-[70%] mx-auto text-black'>
+            <h1 className='text-4xl text-black text-center font-bold my-4'>How to use vite to create react app</h1>
+            <Image className='w-[600px] h-[400px] mx-auto my-4' height={400} width={600} src="/show.jpg" />
+            <p className='text-lg font-bold'>Posted By:  Aaron Dennis</p>
+            <p className='text-lg mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error laborum et repellat! Libero obcaecati cumque quidem, minima nesciunt reprehenderit labore nulla tempore quisquam sequi harum ea voluptatem eligendi ratione inventore.
+                Aliquid nihil libero aspernatur non, maiores quod veritatis exercitationem saepe aperiam laudantium quaerat id. Itaque laborum velit atque, ipsa est optio libero nesciunt? Ex vero tempora alias repudiandae nesciunt est!
+                Minus fugiat nulla optio eum sit reiciendis iusto, temporibus et asperiores deserunt! Suscipit eos repellat ea, error exercitationem corrupti sit aliquid dolorem, blanditiis quis aliquam totam nihil tempore quidem illo.
+                Dolore odio eveniet exercitationem itaque error praesentium veniam debitis? Molestiae, dolor! Possimus, eveniet? Enim dolorum debitis magnam earum deserunt quibusdam excepturi possimus expedita sit, sunt sequi rem consequatur distinctio ducimus!
+                Illo assumenda sapiente delectus dolores quaerat! Aspernatur dicta dolores sunt in animi labore a officia commodi voluptatibus maxime. Natus quia corporis modi similique recusandae qui aliquam dolores cumque unde fuga.
+                Inventore cumque adipisci cum temporibus in nisi doloribus neque quas dolor nobis est tenetur quos quis provident fugit debitis reiciendis accusantium vel, eos saepe delectus. At perferendis quod optio expedita.
+                Illo veritatis sit vel, molestiae itaque illum a laboriosam dolores non corporis velit dolorum magni magnam quam voluptate consequatur soluta quisquam! Ratione repellat, cupiditate dolor omnis fuga quia natus officiis.
+                Totam sunt voluptatem laborum illum recusandae nisi quaerat eveniet provident enim quas esse amet necessitatibus tenetur perspiciatis atque quasi, ex, dolor porro debitis molestias magni ipsam molestiae est perferendis. Nostrum.
+                Incidunt in iste omnis maiores assumenda ex vitae corporis ut dolorum similique commodi distinctio esse illo ea totam suscipit, dolorem facilis, quod illum autem atque. Unde necessitatibus ex nisi fugiat.
+                Odio, quae quos vel tempora, veniam accusantium, labore voluptas itaque expedita modi voluptates eligendi repellendus? Quo aperiam beatae voluptatibus sequi, id unde nobis porro sit ut aliquid, alias, vitae neque!</p>
+        </section>
+    )
+}
+
+export default ShowBlog
