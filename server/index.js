051668b35c644a17bf689c869c4559e4192f179a
AaronDennis07 <aarondennisbv01@gmail.com>
2023-11-14 21:09:16 +0530

    database connection setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log("hi")
+const express = require('express')
+const app = express()
+const {connectDB} = require('./db')
+if(process.env.NODE_ENV==='DEV'){
+    require('dotenv').config()
+   
+}
+    
+app.get("/",(req,res)=>{
+    return res.json("hi")
+})
+
+connectDB(process.env.DATABASE_URL)
+    .then(()=>{
+        console.log("DATABASE CONNECTED")
+        app.listen(process.env.PORT,()=>{
+            console.log(`LISTENING AT PORT ${process.env.PORT}`)
+        })
+    }).catch((err)=>{
+        console.log(err)
+        process.exit(0)
+    })
